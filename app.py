fbd019c70cafd4ee2cfc53ff3ab4dac61ce95e94
Soren <soren@augustos-imac.lan>
2026-04-08 13:18:58 -0400

    chore: remove duplicate init_db calls

--- app.py
+++ app.py
@@ -1426,13 +1426,5 @@ def export_csv():
 with app.app_context():
     init_db()
 
-
-with app.app_context():
-    init_db()
-
-
-with app.app_context():
-    init_db()
-
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
